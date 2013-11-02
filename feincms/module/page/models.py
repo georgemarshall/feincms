38f597a41b838505d933a221a2b18c8f18f10474
George Marshall <george@georgemarshall.name>
2013-11-01 17:41:20 -0700

    Revert "Fixes transaction support to work with Django 1.6"
    This reverts commit 54316ec169365c6561c2f38a53aaf48f46840fdc.

--- feincms/module/page/models.py
+++ feincms/module/page/models.py
@@ -6,11 +6,12 @@ import re
 
 from django.core.cache import cache as django_cache
 from django.conf import settings as django_settings
-from django.db import models, transaction, DEFAULT_DB_ALIAS
+from django.db import models
 from django.db.models import Q, signals
 from django.db.models.loading import get_model
 from django.http import Http404
 from django.utils.translation import ugettext_lazy as _
+from django.db.transaction import commit_on_success
 
 from mptt.models import MPTTModel
 
@@ -230,7 +231,7 @@ class BasePage(create_base_model(MPTTModel), ContentModelMixin):
         # determine whether it has been changed in the save handler:
         self._original_cached_url = self._cached_url
 
-    @transaction.commit_on_success_unless_managed(DEFAULT_DB_ALIAS)
+    @commit_on_success
     def save(self, *args, **kwargs):
         """
         Overridden save method which updates the ``_cached_url`` attribute of
@@ -276,7 +277,7 @@ class BasePage(create_base_model(MPTTModel), ContentModelMixin):
             super(BasePage, page).save() # do not recurse
     save.alters_data = True
 
-    @transaction.commit_on_success_unless_managed(DEFAULT_DB_ALIAS)
+    @commit_on_success
     def delete(self, *args, **kwargs):
         super(BasePage, self).delete(*args, **kwargs)
         self.invalidate_cache()
